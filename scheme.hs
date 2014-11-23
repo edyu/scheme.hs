0e4431f8aeb81b7ea4c8bf7007a762152026fb66
Ed Yu <edlyuu@gmail.com>
2014-11-23 17:25:28 -0600

    Implement recursive parsers for lists, dotted lists, and quoted datums.

--- scheme.hs
+++ scheme.hs
@@ -34,20 +34,40 @@ parseAtom = do
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
          <|> parseString
          <|> parseNumber
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value"
-
-spaces :: Parser ()
-spaces = skipMany1 space
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn (readExpr (args !! 0))
+    args <- getArgs
+    putStrLn (readExpr (args !! 0))
