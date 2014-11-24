c292b4640d9e6667c6d2d40a7a5b0b968fa47887
Ed Yu <edlyuu@gmail.com>
2014-11-23 20:11:55 -0600

    Do the exercise for the recursive parser.

---
+++ step3/ex3.hs
@@ -0,0 +1,70 @@
+module Main where
+
+import Control.Monad
+import System.Environment
+import Text.ParserCombinators.Parsec
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                        "#t" -> Bool True
+                        "#f" -> Bool False
+                        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+spaces1 :: Parser ()
+spaces1 = skipMany1 space
+
+parseList :: Parser LispVal
+parseList = do char '(' >> spaces
+               head <- parseExpr `sepEndBy` spaces1
+               do char '.' >> spaces1
+                  tail <- parseExpr
+                  spaces >> char ')'
+                  return $ DottedList head tail
+                 <|> (spaces >> char ')' >> (return $ List head))
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+         <|> parseQuoted
+         <|> parseList
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+main :: IO ()
+main = do
+    args <- getArgs
+    putStrLn (readExpr (args !! 0))
